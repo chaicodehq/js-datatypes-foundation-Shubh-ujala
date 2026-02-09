6c142e252dfe736a680d7427ea8d9dfb2394a940
shubh ujala <shubhujala.code@gmail.com>
2026-02-09 18:09:21 +0530

    solve 2nd problem

--- src/02-rangoli-maker.js
+++ src/02-rangoli-maker.js
@@ -47,20 +47,43 @@
  */
 export function repeatPattern(pattern, times) {
   // Your code here
+  if(typeof(pattern)!= 'string' || !Number.isInteger(times)) return "";
+
+  if(times <= 0) return "";
+  let strPattern = "";
+  for(let i = 0 ; i<times ; i++){
+    strPattern += pattern;
+  }
+  return strPattern;
 }
 
 export function extractRangoliCenter(design, start, end) {
   // Your code here
+  if(typeof(design)!='string' || typeof(start)!='number' || typeof(end)!= 'number') return "";
+
+  return design.slice(start,end);
+
 }
 
 export function splitAndJoinRangoli(colorString, oldSep, newSep) {
   // Your code here
+  if(typeof(colorString)!='string' || typeof(oldSep)!='string'|| typeof(newSep)!='string') return "";
+  return colorString.split(oldSep).join(newSep);
 }
 
 export function replaceRangoliColor(design, oldColor, newColor) {
   // Your code here
+  if(typeof(design)!='string' || typeof(oldColor)!='string' || typeof(newColor)!='string'){
+    return "";
+  }
+  return design.replaceAll(oldColor,newColor);
 }
 
 export function makeRangoliBorder(char, length) {
   // Your code here
+  if(typeof(char)!='string' || length<=0){
+    return "";
+  }
+  let pattern = char.repeat(length);
+  return pattern.slice(0,length);
 }
